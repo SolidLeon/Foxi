3188d69a71456c51252b7b5c9f1ed292a82d99f2
SolidLeon <markusmannel@gmail.com>
2015-06-03 13:15:21 +0200

    util hexOut added custom PrintStream for output

--- src/foxi/framework/Utils.java
+++ src/foxi/framework/Utils.java
@@ -1,13 +1,15 @@
 package foxi.framework;
 
+import java.io.PrintStream;
+
 public class Utils {
-	public static void hexOut(int[] data) {
+	public static void hexOut(PrintStream out, int[] data) {
 		StringBuilder sb1 = new StringBuilder();
 		StringBuilder sb2 = new StringBuilder();
 		int i = 0;
 		for (i = 0; i < data.length; i++) {
 			if (i % 0x10 == 0 && sb1.length() > 0) {
-				System.out.printf("%04X:  %-64s    %s%n", (i - 0x10), sb1.toString(), sb2.toString());
+				out.printf("%04X:  %-64s    %s%n", (i - 0x10), sb1.toString(), sb2.toString());
 				sb1.setLength(0);
 				sb2.setLength(0);
 			}
@@ -15,7 +17,10 @@ public class Utils {
 			sb2.append(Character.isISOControl((char) data[i]) ? '.' : (char) data[i]);
 		}
 		if (sb1.length() > 0) {
-			System.out.printf("%04X:  %-64s    %s%n", (i - 0x10), sb1.toString(), sb2.toString());
+			out.printf("%04X:  %-64s    %s%n", (i - 0x10), sb1.toString(), sb2.toString());
 		}
+	}
+	public static void hexOut(int[] data) {
+		hexOut(System.out, data);
 	}
 }
